608d9a9b21f371a26bca1ea36c74c5d3b45eacf0
Gaurav Patel <gauravp.itpath@gmail.com>
2024-12-05 15:11:35 +0530

    Changes added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-// App.js
-import React from 'react';
-import Hero from './components/hero';
-import Clients from './components/clients';
-import CoworkingSpace from './components/coworking-space';
-import Decoration from './components/decoration';
-import Pricing from './components/pricing';
-import Customers from './components/customers';
-import Blogs from './components/Blogs';
-import Subscription from './components/subscription';
-import Footer from './components/footer';
-import LocationCard from './components/Location';
+import React, { Suspense } from "react";
+
+// Lazy loading the components
+const Hero = React.lazy(() => import("./components/hero"));
+const Clients = React.lazy(() => import("./components/clients"));
+const CoworkingSpace = React.lazy(() => import("./components/coworking-space"));
+const Decoration = React.lazy(() => import("./components/decoration"));
+const Pricing = React.lazy(() => import("./components/pricing"));
+const Customers = React.lazy(() => import("./components/customers"));
+const Blogs = React.lazy(() => import("./components/Blogs"));
+const Subscription = React.lazy(() => import("./components/subscription"));
+const Footer = React.lazy(() => import("./components/footer"));
+const LocationCard = React.lazy(() => import("./components/Location"));
 
 function App() {
   return (
     <div className="w-full min-h-screen overflow-x-hidden">
-      <Hero />
-      <Clients />
-      <CoworkingSpace/>
-      <Decoration/>
-      <Pricing/>
-      <Customers/>
-      <Blogs/>
-      <LocationCard/>
-      <Subscription/>
-      <Footer/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Hero />
+        <Clients />
+        <CoworkingSpace />
+        <Decoration />
+        <Pricing />
+        <Customers />
+        <Blogs />
+        <LocationCard />
+        <Subscription />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
